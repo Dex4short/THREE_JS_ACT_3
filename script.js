049821fbb9e9d7cb2c17e10ed6aa417be9c9d43d
Dexter Pacaña <dextepacanya@gmail.com>
2024-12-09 18:14:32 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,66 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { FontLoader } from 'three/addons/loaders/FontLoader.js';
+import { TextGeometry } from 'three/addons/geometries/TextGeometry.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+
+/*Canvas*/
+const canvas = document.querySelector('canvas.webgl');
+
+/*Scene*/
+const scene = new THREE.Scene();
+
+/*Camera*/
+const sizes = {width: window.innerWidth, height: window.innerHeight};
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+scene.add(camera);
+
+/*Renderer*/
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
+
+// Controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
+/*Resize Listener*/
+window.addEventListener('resize', () => {
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+  
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+  
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.render(scene, camera);
+});
+
+/*Objects*/
+const loader = new GLTFLoader();
+const path = 'https://dex4short.github.io/THREE_JS_ACT_3/hatsune_miku/scene.png';
+loader.load(
+  path,
+  (gltf) => {
+    const model = gltf.scene;
+    scene.add(model);
+    model.position.set(0, 0, 0);
+    model.scale.set(1, 1, 1);
+  },
+  (xhr) => console.log((xhr.loaded / xhr.total) * 100 + '% loaded'),
+  (error) => console.error('An error occurred:', error)
+);
+
+/*Animation*/
+const tick = function() {
+    renderer.render(scene, camera);
+    controls.update();
+    window.requestAnimationFrame(tick);
+};
+tick();
+
+/*GUI*/
